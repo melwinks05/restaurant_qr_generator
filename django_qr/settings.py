0ef4cb0e0ff6c0b1d1ccf4966b9e298c1e65917b
Melwin K S <melwinks75@gmail.com>
2026-08-08 20:09:46 +0530

    updated requirements.txt

--- django_qr/settings.py
+++ django_qr/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS=localhost,127.0.0.1, restaurant-qr-generator.onrender.com
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'restaurant-qr-generator.onrender.com']
+
 
 
 # Application definition
